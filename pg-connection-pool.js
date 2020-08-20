9e21b0df79788571dc3f979871b70cff87f8ac26
J.Nicewander <jnicewander@live.com>
2020-08-20 12:29:25 -0400

    Credentials added for database connection.

--- pg-connection-pool.js
+++ pg-connection-pool.js
@@ -1,1 +1,12 @@
-// const pool = require('pg');
+const pool = require('pg');
+
+const credentials = new Pool({
+  user: "postgres",
+  password: "GC_PASSWORD",
+  host: "localhost",
+  port: 5433,
+  database: "ExpressShopDB",
+  ssl: false,
+});
+
+module.exports = credentials;
