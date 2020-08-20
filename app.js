ca4a16dd865cf7ab4f095f5b5916ecfda12e7001
J.Nicewander <jnicewander@live.com>
2020-08-20 13:37:40 -0400

    Added references to new database connection.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const app = express();
 const cartItems = require('./cart-items');
+const expressShopDB = require('./express-shop-db');
 const cors = require('cors');
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use('/', expressShopDB)
 app.use('/cart-items', cartItems);
 
 app.get('*', (req, res) => {
